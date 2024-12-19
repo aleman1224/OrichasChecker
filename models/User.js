47991ece01b954c132be216fa62ab8599a7dd419
SoulM4chine12 <Efrnalgol@gmail.com>
2024-12-19 04:46:50 -0400

    Simplificado modelo User sin bcrypt

--- models/User.js
+++ models/User.js
@@ -1,6 +1,5 @@
 // models/User.js
 import mongoose from 'mongoose';
-import bcrypt from 'bcrypt';
 
 const userSchema = new mongoose.Schema({
     username: {
@@ -41,21 +40,9 @@ const userSchema = new mongoose.Schema({
     }
 });
 
-// Hash de contraseña antes de guardar
-userSchema.pre('save', async function(next) {
-    if (!this.isModified('password')) return next();
-    try {
-        const salt = await bcrypt.genSalt(10);
-        this.password = await bcrypt.hash(this.password, salt);
-        next();
-    } catch (error) {
-        next(error);
-    }
-});
-
-// Método para comparar contraseñas
+// Método para comparar contraseñas (versión simple)
 userSchema.methods.comparePassword = function(candidatePassword) {
-    return bcrypt.compare(candidatePassword, this.password);
+    return this.password === candidatePassword;
 };
 
 export const User = mongoose.model('User', userSchema);
